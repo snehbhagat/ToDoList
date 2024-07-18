f3d922ab452bd1f9577e1a96fb604cf86952a573
Sneh Bhagat <snehbhagat12@gmail.com>
2024-07-18 10:13:11 +0530

    Fixed CSS

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import TodoList from './To-do-list';
 import Header from './header';
 import Hero from './To-Do-hero';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit , faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [inputValue , setInputValue] = useState('');
 
   const completedTodos = todos.filter(todo => todo.completed).length;
 
